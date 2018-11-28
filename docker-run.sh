a694795b4c6b0cfc5254b4690f4c291de0b9bd42
Mehdi El Gueddari <emehdi@gmail.com>
2018-11-28 11:17:22 +0000

    Add some explanations of the docker commands.

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -p 80:5100 statuscoder statuscoder
+# This relies on our docker image having been built with a name of 'statuscoder'.
+# --name is the name you want to give the container you're starting. It can be anything you want.
+# --publish defines on which local port (of the host machine) our application be bound.
+docker run --name statuscoder --publish 80:5100 statuscoder
